e49fd24d0d10cb31f91a4824c72ad5bf92bb32f6
Ben Hessel <hesselfitness@gmail.com>
2022-07-18 18:48:14 -0400

    counter up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(1);
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Virtual DOM Demo!</h1>
+      <p>Right now we have 1 dog.</p>
+      <p>Guess how many more my girlfriend thinks we need?</p>
+      <p>{count}</p>
+      <button onClick={decrease}> - </button>
+      <button onClick={increase}> + </button>
+      <p>...add dogs?</p>
+      <button>Definitely</button>
     </div>
   );
 }
